f3437e1d7251b9c3ff73abb2937adc04d91d1b1c
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-10-13 17:50:09 -0500

    10 - ¿Qué es el hoisting?

---
+++ src/hoisting/index.js
@@ -0,0 +1,15 @@
+/**  Prueba de hoisting1 */
+a = 2;
+var a;
+console.log(a);
+/**  Prueba de hoisting2 */
+console.log(a);
+var a = 2;
+
+
+nameOfDog('Manuel')
+
+function nameOfDog(name){
+    console.log(name)
+}
+
